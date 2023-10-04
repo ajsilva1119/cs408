397269b6a503162b1d6ad3a7ce0aba0582323c98
Alex Silva <alexsilva@u.boisestate.edu>
2023-10-03 21:48:41 -0600

    new

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ https://disctalk408-60d24c8b200d.herokuapp.com/<!DOCTYPE html>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Mock-up Website</title>
-    <link rel="stylesheet" type="text/css" href="stylesheet1.css">
+    <link rel="stylesheet" type="text/css" href="stylesheets\stylesheet1.css">
 </head>
 <body>
     <header>
