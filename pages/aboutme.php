59b509e64a02cda57a2baf0877482b86b909acbd
Alex Silva <alexsilva@u.boisestate.edu>
2023-10-03 23:12:50 -0600

    new

--- pages/aboutme.php
+++ pages/aboutme.php
@@ -6,13 +6,14 @@
     <title>Welcome to Disc Golf Fourms!</title>
     <link rel="icon" href="favicon.ico" type="image/x-icon">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" type="text/css" href="stylesheets\stylesheet1.css">
+    <link rel="stylesheet" type="text/css" href="../stylesheets\stylesheet1.css">
 </head>
 <body>
     <nav>
         <ul>
-            
+            <img src="../Bronco.png" alt="logo" width="25" height="25" style="float:left">
             <form>
+                <li><a href="../index.php">Home</a></li> 
                 <li><a href="#">Search</a></li> 
                 <input type="text" name="search"> <input type="submit" name="searchbutton">
                 <li><a href="#">Sign In</a></li>
@@ -21,11 +22,22 @@
         </ul>
     </nav>
     <header class="style-header">
-        <h1>Welcome to My Mock-up Website</h1>
-        <img src="Bronco.png" alt="logo" width="100" height="100" class="style-image"> 
+        <h1>About Me</h1>
+        <p> I really like disc golf </p> 
+        
     </header>
+    <footer>
+            <p>Likes 12 </p>
+    </footer>
     <nav>
     </nav>
+    <div>
+        <section class="section">
+            <h2>Commet one</h2>
+            <ul>
+                <p> I agree!</p>
+            </ul>
+        </section >
    
     </div>
     <footer>
